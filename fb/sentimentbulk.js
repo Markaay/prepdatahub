76a4fc455ca9ffbfdc8edb77ceb4764f318b86f0
Mark <Ebergen@MacBook-Air-van-Mark.local>
2018-06-13 07:29:04 +0200

    10 as minimum

--- fb/sentimentbulk.js
+++ fb/sentimentbulk.js
@@ -44,7 +44,7 @@ mysqlpromise.createConnection({
         //loop through every comment from the query
         for(k=0;k<rows.length;k++){
             //only analyze comments longer than 20 characters
-            if(rows[k].comment_message.length > 25){
+            if(rows[k].comment_message.length > 10){
                 console.log(k + " van " + messageCount);
                 //console.log(rows[k].comment_message);
                 let message = rows[k].comment_message;
